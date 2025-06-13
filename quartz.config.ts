b5780b4b561f626c6ef0ec634ebc7bb457dfa2c6
ㅤ <muctebanesiri@gmail.com>
2025-06-13 03:45:30 +0330

    Quartz sync: Jun 13, 2025, 3:45 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "مجتبی",
+    pageTitle: "موجتبا",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
