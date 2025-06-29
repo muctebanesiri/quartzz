94766516de912c61a050134de7e90a6f885ceaee
ㅤ <muctebanesiri@gmail.com>
2025-06-29 19:24:20 +0330

    update

--- quartz.config.ts
+++ quartz.config.ts
@@ -65,7 +65,6 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.MultiLanguage(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
