621ec1146232fd691e79883c0c6353c78a6d6390
ㅤ <muctebanesiri@gmail.com>
2025-07-15 20:22:23 +0330

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,94 +1,24 @@
-import { QuartzConfig } from "./quartz/cfg"
-import * as Plugin from "./quartz/plugins"
+## Podcast Studio
 
-/**
- * Quartz 4.0 Configuration
- *
- * See https://quartz.jzhao.xyz/configuration for more information.
- */
-const config: QuartzConfig = {
-  configuration: {
-    pageTitle: "مجتبی",
-    enableSPA: true,
-    enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    locale: "fa-IR",
-    baseUrl: "mucteba.ir",
-    ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
-    theme: {
-      fontOrigin: "googleFonts",
-      cdnCaching: true,
-      typography: {
-        header: "Vazirmatn",
-        body: "Vazirmatn",
-        code: "IBM Plex Mono",
-      },
-      colors: {
-        lightMode: {
-          light: "#F8F8F8",
-          lightgray: "rgb(242, 240, 229)",
-          gray: "rgb(206, 205, 195)",
-          darkgray: "rgb(16, 15, 15)",
-          dark: "rgb(16, 15, 15)",
-          secondary: "rgb(139, 126, 200)",
-          tertiary: "rgb(94, 64, 157)",
-          highlight: "rgba(94, 64, 157, 0.15)",
-          textHighlight: "rgba(94, 64, 157, 0.15)",
-        },
-        darkMode: {
-          light: "rgb(16, 15, 15)",
-          lightgray: "rgb(40, 39, 38)",
-          gray: "#9f9898",
-          darkgray: "rgb(214, 211, 203)",
-          dark: "rgb(206, 205, 195)",
-          secondary: "#a68adf",
-          tertiary: "#846aff",
-          highlight: "rgba(139, 126, 200, 0.15)",
-          textHighlight: "rgba(139, 126, 200, 0.15)",
-        },
-      },
-    },
-  },
-  plugins: {
-    transformers: [
-      Plugin.FrontMatter(),
-      Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
-      }),
-      Plugin.SyntaxHighlighting({
-        theme: {
-          light: "github-light",
-          dark: "github-dark",
-        },
-        keepBackground: false,
-      }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
-      Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
-      Plugin.Description(),
-      Plugin.Latex({ renderEngine: "katex" }),
-      // Plugin.HardLineBreaks(),
-    ],
-    filters: [Plugin.RemoveDrafts()],
-    emitters: [
-      Plugin.AliasRedirects(),
-      Plugin.ComponentResources(),
-      Plugin.ContentPage(),
-      Plugin.FolderPage(),
-      Plugin.TagPage(),
-      Plugin.ContentIndex({
-        enableSiteMap: true,
-        enableRSS: true,
-      }),
-      Plugin.Assets(),
-      Plugin.Static(),
-      Plugin.NotFoundPage(),
-    ],
-  },
-}
+This is a template for a podcast website. It is built with:
+- [Next.js](https://nextjs.org/)
+- [Tailwind CSS](https://tailwindcss.com/)
 
-export default config
+## Getting Started
+
+1. First, you'd need a podcast RSS url and the final production domain URL for your website.
+
+```dotenv
+NEXT_PUBLIC_PODCAST_RSS="yoururl"
+NEXT_PUBLIC_OG_URL="https://example.com"
+```
+
+2. Open `podcast.config.ts` to configure your podcast.
+
+## Running Locally
+```bash
+pnpm install
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
