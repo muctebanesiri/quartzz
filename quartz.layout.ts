721506b4cdb9913978711a58dff19bfc24379238
ㅤ <muctebanesiri@gmail.com>
2025-08-02 18:46:30 +0330

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,7 +31,6 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.RecentNotes({ title: "Recent writing" })
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
