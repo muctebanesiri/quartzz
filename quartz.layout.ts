fdf999864f2df2e20b51550deef749aac2a2a8a9
ㅤ <muctebanesiri@gmail.com>
2025-08-02 18:54:01 +0330

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -63,7 +63,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],Component.RecentNotes({ title: "Recent writing" }),
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],Component.RecentNotes(),
   left: [
     Component.MobileOnly(Component.PageTitle()),
     Component.MobileOnly(Component.Darkmode()),
