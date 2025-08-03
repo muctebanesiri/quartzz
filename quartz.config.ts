10c74dede26b456df7b08e44646ad9c0e3238ee3
ㅤ <muctebanesiri@gmail.com>
2025-08-03 17:59:34 +0330

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "fa-IR", 
+    locale: "en-USA",
     baseUrl: "mucteba.ir",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
